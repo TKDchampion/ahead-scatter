d78faafde049aae7508ba3d2c0701f7da70a3935
TKDchampion <as1219123@yahoo.com.tw>
2025-01-13 08:49:30 +0800

    create a ScatterPlot to show xAxes yAxes title and chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import fs from "fs";
 import path from "path";
+import * as d3 from "d3";
+import ScatterPlotWrapper from "@/components/ScatterPlot";
+
+const xAxes = "CD45-KrO";
+const yAxes = "SS INT LIN";
+const title = "Cell Distribution (CD45+)";
 
 export default async function Home() {
   const filePath = path.join(process.cwd(), "public", "CD45_pos.csv");
   const fileContents = fs.readFileSync(filePath, "utf-8");
-  console.log(fileContents);
+  const rows = d3.csvParse(fileContents);
+  const data = rows.map((row) => ({
+    [xAxes]: +row[xAxes],
+    [yAxes]: +row[yAxes],
+  }));
 
   return (
     <div className="bg-white h-full text-black p-8">
       <h1>Scatter Plot</h1>
+      <ScatterPlotWrapper
+        data={data}
+        xAxes={xAxes}
+        yAxes={yAxes}
+        title={title}
+      />
     </div>
   );
 }
